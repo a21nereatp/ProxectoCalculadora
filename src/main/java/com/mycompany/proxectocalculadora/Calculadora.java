92758d5a4a473a04a1d251e6e236037df9818678
Nerea Trillo <a21nereatp@iessanclemente.net>
2022-06-09 20:57:45 +0200

    Clase Calculadora completada

--- src/main/java/com/mycompany/proxectocalculadora/Calculadora.java
+++ src/main/java/com/mycompany/proxectocalculadora/Calculadora.java
@@ -4,10 +4,56 @@
  */
 package com.mycompany.proxectocalculadora;
 
+import java.util.logging.Logger;
+
 /**
  *
  * @author Usuario
  */
 public class Calculadora {
-    
+  
+    private int num1;
+
+    private int num2;
+
+    public Calculadora() {
+    }
+
+    public Calculadora(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+
+    public int getNum1() {
+        return num1;
+    }
+
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+   public int suma(){
+    int resultado=num1+num2;
+    return resultado;
+   }
+   public int resta(){
+       int resultado=num1-num2;
+       return resultado;
+   }
+   public int multiplicacion(){
+       int resultado= num1*num2;
+       return resultado;
+   }
+   public int division(){
+       int resultado= num1/num2;
+       return resultado;
+   }
+  
 }
