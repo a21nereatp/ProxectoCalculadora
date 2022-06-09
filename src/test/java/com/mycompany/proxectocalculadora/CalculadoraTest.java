812109f5c84a647c294c793039c4bf1c27709c27
Nerea Trillo <a21nereatp@iessanclemente.net>
2022-06-09 21:13:36 +0200

    Los 4 tests pasan

--- src/test/java/com/mycompany/proxectocalculadora/CalculadoraTest.java
+++ src/test/java/com/mycompany/proxectocalculadora/CalculadoraTest.java
@@ -17,81 +17,39 @@ public class CalculadoraTest {
     }
 
     @Test
-    public void testGetNum2() {
-        System.out.println("getNum2");
-        Calculadora instance = new Calculadora();
-        int expResult = 0;
-        int result = instance.getNum2();
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
-    }
-
-    @Test
-    public void testSetNum2() {
-        System.out.println("setNum2");
-        int num2 = 0;
-        Calculadora instance = new Calculadora();
-        instance.setNum2(num2);
-        fail("The test case is a prototype.");
-    }
-
-    @Test
-    public void testGetNum1() {
-        System.out.println("getNum1");
-        Calculadora instance = new Calculadora();
-        int expResult = 0;
-        int result = instance.getNum1();
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
-    }
-
-    @Test
-    public void testSetNum1() {
-        System.out.println("setNum1");
-        int num1 = 0;
-        Calculadora instance = new Calculadora();
-        instance.setNum1(num1);
-        fail("The test case is a prototype.");
-    }
-
-    @Test
     public void testSuma() {
         System.out.println("suma");
-        Calculadora instance = new Calculadora();
-        int expResult = 0;
+        Calculadora instance = new Calculadora(16,4);
+        int expResult = 20;
         int result = instance.suma();
         assertEquals(expResult, result);
-        fail("The test case is a prototype.");
     }
 
     @Test
     public void testResta() {
         System.out.println("resta");
-        Calculadora instance = new Calculadora();
-        int expResult = 0;
+        Calculadora instance = new Calculadora(16,4);
+        int expResult = 12;
         int result = instance.resta();
         assertEquals(expResult, result);
-        fail("The test case is a prototype.");
     }
 
     @Test
     public void testMultiplicacion() {
         System.out.println("multiplicacion");
-        Calculadora instance = new Calculadora();
-        int expResult = 0;
+        Calculadora instance = new Calculadora(16,4);
+        int expResult = 64;
         int result = instance.multiplicacion();
         assertEquals(expResult, result);
-        fail("The test case is a prototype.");
     }
 
     @Test
     public void testDivision() {
         System.out.println("division");
-        Calculadora instance = new Calculadora();
-        int expResult = 0;
+        Calculadora instance = new Calculadora(16,4);
+        int expResult = 4;
         int result = instance.division();
         assertEquals(expResult, result);
-        fail("The test case is a prototype.");
     }
     
 }
